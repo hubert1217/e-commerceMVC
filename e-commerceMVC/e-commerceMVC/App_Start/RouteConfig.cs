702413c6ee3162a9a61eb3496c364cbedf857832
hubert1217 <hubert.roj@gmail.com>
2016-04-30 17:14:12 +0200

    dodanie tras routingu

--- e-commerceMVC/e-commerceMVC/App_Start/RouteConfig.cs
+++ e-commerceMVC/e-commerceMVC/App_Start/RouteConfig.cs
@@ -14,6 +14,18 @@ namespace e_commerceMVC
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name:"ProductDetails",
+                url:"album-{id}.html",
+                defaults: new {controller = "Store", action = "Details" }
+            );
+
+            routes.MapRoute(
+                name: "StaticPage",
+                url: "strony/{viewname}.html",
+                defaults: new { controller = "Home", action = "StaticContent" }
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
