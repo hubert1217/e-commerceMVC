5d4ff9d2a6ee5017aebdebd96de2bb69e8ac225f
hubert1217 <hubert.roj@gmail.com>
2016-05-15 15:20:00 +0200

    Skończony koszyk

---
+++ e-commerceMVC/e-commerceMVC/Controllers/CartController.cs
@@ -0,0 +1,70 @@
+﻿using e_commerceMVC.DAL;
+using e_commerceMVC.Infrastructure;
+using e_commerceMVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace e_commerceMVC.Controllers
+{
+    public class CartController : Controller
+    {
+        //
+        // GET: /Cart/
+
+        private ShoppingCartManager shoppingCartManager;
+        private ISessionManager sessionManager { get; set; }
+        private StoreContext db = new StoreContext();
+
+
+        public CartController() 
+        {
+            this.sessionManager = new SessionManager();
+            this.shoppingCartManager = new ShoppingCartManager(this.sessionManager, this.db);
+            
+        }
+
+
+        public ActionResult Index()
+        {
+            var cartItems = shoppingCartManager.GetCart();
+            var cartTotalPrice = shoppingCartManager.GetCartTotalPrice();
+
+            CartViewModel cartVM = new CartViewModel() { CartItems = cartItems, TotalPrice = cartTotalPrice };
+            return View(cartVM);
+        }
+
+
+        public ActionResult AddToCart(int id) 
+        {
+            shoppingCartManager.AddToCart(id);
+
+            return RedirectToAction("Index");
+        }
+
+        public int GetCartItemsCount() 
+        {
+            return shoppingCartManager.GetCartItemsCount();
+        }
+
+        public ActionResult RemoveFromCart(int productID) 
+        {
+            int itemCount = shoppingCartManager.RemoveFromCart(productID);
+            int cartItemsCount = shoppingCartManager.GetCartItemsCount();
+            decimal cartTotal = shoppingCartManager.GetCartTotalPrice();
+
+            var result = new CartRemoveViewModel {
+            
+            RemoveItemId=productID,
+            RemovedItemCount = itemCount,
+            CartTotal = cartTotal,
+            CartItemsCount=cartItemsCount
+            };
+
+            return Json(result);
+        }
+	}
+
+}
---
+++ e-commerceMVC/e-commerceMVC/ViewModels/CartViewModel.cs
@@ -0,0 +1,15 @@
+﻿using e_commerceMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace e_commerceMVC.ViewModels
+{
+    public class CartViewModel
+    {
+        public List<CartItem> CartItems { get; set; }
+
+        public decimal TotalPrice { get; set; }
+    }
+}
