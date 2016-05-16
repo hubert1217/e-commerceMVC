14f72c9fa407a8f4d1b18a98537544c36d4bf0d4
hubert1217 <hubert.roj@gmail.com>
2016-05-16 20:42:46 +0200

    Form Logowania

---
+++ e-commerceMVC/e-commerceMVC/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using e_commerceMVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace e_commerceMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model, string returnUrl) 
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else 
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+	}
+}
---
+++ e-commerceMVC/e-commerceMVC/ViewModels/AccountViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace e_commerceMVC.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage="Musisz wpriwadzić e-mail")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Musisz wpriwadzić hasło")]
+        public string Password { get; set; }
+        [Display(Name="Zapamiętaj mnie")]
+        public bool RememberMe { get; set; }
+    }
+}
